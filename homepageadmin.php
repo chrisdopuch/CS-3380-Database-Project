42c767c1512cde270cc78dea168f52df010866d0
Breona Butler <breona.butler@gmail.com>
2013-04-08 15:11:35 -0500

    Added homepage for admin and participant

---
+++ homepageadmin.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title> Home </title>
+<style type = "text/css">
+
+	Welcome to the Physcology Research Department
+	
+	<img src = "banner.jpg " 
+
+
+</style>
+</head>
+<body>
+
+	<div id = "topPannel" class = "group">
+		Start here
+</body>
+</html>
+		
---
+++ homepagepart.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+<title> Home </title>
+<style type = "text/css">
+
+#top
+{
+		text-align: center;
+		border: 1px solid blue;
+		padding-left: 10px;
+		background-color: #FFD651;
+		text-color: black;
+}
+
+#Options
+{
+	text-align: left;
+	padding-right: 30px;
+	background-color: white;
+{
+
+<img src = "banner.jpg "/>
+
+</style>
+</head>
+<body>
+
+	<div id = "banner">
+	
+		<img src = "banner.jpg " alt "Mizzou Physcology"/>
+	
+	<div id = "top">
+	
+		<a href = " ">My Studies</a>
+		&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href = " ">All Studies</a>
+		&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href = " ">My Profile</a>
+		&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href = " ">Logout</a>
+		
+	<div id = "Options">
+	
+			<div id = "MyStudies">
+			
+				<a href = " ">My Studies</a>
+					</br>
+							&nbsp;&nbsp;&nbsp;&nbsp;
+					-View and edit your studies
+					</br>
+							&nbsp;&nbsp;&nbsp;&nbsp;
+					-View, add, or change timeslots
+		
+				<div id = "AllStudies">
+					
+					<a href = " ">All Studies</a>
+					</br>
+							&nbsp;&nbsp;&nbsp;&nbsp;
+						-View all studies available
+					</br>
+					</br>
+		
+					<div id = "MyProfile">
+						<a href = " ">My Profile</a>
+						</br>
+								&nbsp;&nbsp;&nbsp;&nbsp;
+							-Change your password
+						</br>
+								&nbsp;&nbsp;&nbsp;&nbsp;
+							-Modify contact information
+						</br>
+								&nbsp;&nbsp;&nbsp;&nbsp;
+							-Change your email address
+						</br>
+						</br>
+
+</body>
+</html>
+		
