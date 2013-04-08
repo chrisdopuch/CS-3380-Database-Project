e8a914489ec6b0759f069bbe9c0bac0e5415357f
Breona Butler <breona.butler@gmail.com>
2013-04-08 15:13:58 -0500

    Updated homepage for admin
    The first upload was the original, accidently uploaded that one and not
    finished page

--- homepageadmin.php
+++ homepageadmin.php
@@ -3,17 +3,93 @@
 <title> Home </title>
 <style type = "text/css">
 
-	Welcome to the Physcology Research Department
-	
-	<img src = "banner.jpg " 
+#top
+{
+		text-align: center;
+		border: 1px solid blue;
+		padding-left: 10px;
+		background-color: #FFD651;
+		text-color: black;
+}
 
+#Options
+{
+	text-align: left;
+	padding-right: 30px;
+	background-color: white;
+{
+
+<img src = "banner.jpg "/>
 
 </style>
 </head>
 <body>
 
-	<div id = "topPannel" class = "group">
-		Start here
+	<div id = "banner">
+	
+		<img src = "banner.jpg " alt "Mizzou Physcology"/>
+	
+	<div id = "top">
+	
+		<a href = " ">My Studies</a>
+		&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href = " ">All Studies</a>
+		&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href = " ">Add New Study</a>
+		&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href = " ">My Profile</a>
+		&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href = " ">Logout</a>
+		
+	<div id = "Options">
+	
+			<div id = "MyStudies">
+			
+				<a href = " ">My Studies</a>
+					</br>
+							&nbsp;&nbsp;&nbsp;&nbsp;
+					-View and edit your studies
+					</br>
+							&nbsp;&nbsp;&nbsp;&nbsp;
+					-View, add, or change timeslots
+					</br>
+							&nbsp;&nbsp;&nbsp;&nbsp;
+					-Grant or deny participant credit
+					</br>
+					</br>
+		
+				<div id = "AllStudies">
+					
+					<a href = " ">All Studies</a>
+					</br>
+							&nbsp;&nbsp;&nbsp;&nbsp;
+						-View all studies available to participants
+					</br>
+					</br>
+				
+				<div id = "AddNewStudy">
+				
+					<a href = " ">Add New Study</a>
+					</br>
+							&nbsp;&nbsp;&nbsp;&nbsp;
+						-Create a new study
+					</br>
+					</br>
+		
+					<div id = "MyProfile">
+						<a href = " ">My Profile</a>
+						</br>
+								&nbsp;&nbsp;&nbsp;&nbsp;
+							-Change your password
+						</br>
+								&nbsp;&nbsp;&nbsp;&nbsp;
+							-Modify contact information
+						</br>
+								&nbsp;&nbsp;&nbsp;&nbsp;
+							-Change your email address
+						</br>
+						</br>
+
 </body>
 </html>
 		
