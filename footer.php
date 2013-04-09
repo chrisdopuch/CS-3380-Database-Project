305afb77ae9b510490c56dca366811c3bd10587f
Chris Dopuch <cdp73@mail.missouri.edu>
2013-04-09 13:43:30 -0500

    Modified footer
    edited footer and style

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <!-- this is the footer that will be displayed on the bottom of all the pages in our website -->
-<hr />
+
 <footer>
+	<br />
 	<p>Site designed and developed by: Breona Butler, Chris Dopuch, Adam Faszl, Corey Jumper, and Benjamin Riemann</p>
+	<br />
 	<p>Copyright 2013</p>
 </footer>
