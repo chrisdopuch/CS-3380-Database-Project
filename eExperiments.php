1860b8b7d40518f2767cf90fe81272554379f62e
Adam Faszl <ajfaszl@mail.missouri.edu>
2013-04-25 02:40:24 -0500

    eExperiments.php version update
    updated to correct version from an old version that had made it into the
    repo

--- eExperiments.php
+++ eExperiments.php
@@ -2,8 +2,7 @@
 
 <!DOCTYPE html>
 <!--connect to the database and start the session-->
-<?php include 'connect.php';
-session_start();?>
+<?php include 'connect.php';?>
 <head>
 <title>Experiments</title>
 <!--include the style sheet for the website-->
@@ -15,27 +14,123 @@ session_start();?>
 top("experimenter");
 ?>
 <div id='main' class='clearfix'>
-	<h2>Reports</h2><br />
+	<h2>Experiments</h2><br />
 
 <?php
-	$query = "SELECT * FROM database.experiments ORDER BY expid";
+		$action = $_GET['action']; // edit, remove, edit_commit, NOTHING
+		$val = $_GET['val']; //expid
+
+		switch ($action){
+			//this action shows an editable form to the user
+			case "edit":
+				//define the query to select the country of interest
+				$query = "SELECT * FROM database.experiments WHERE expid = $1";
+				//prepare the query
+				$stmt = pg_prepare($conn, "select_exp", $query);
+				//execute query with the desired country code
+				$result = pg_execute($conn, "select_exp", array($val));
+				//print the edit form
+				pgResultsToEditableTableForm($result, $type, $val);
+				break;
+
+			//this action removes the selected experiment from the database
+			case "remove":
+				$query = "DELETE FROM database.experiments WHERE expid = $1";
+				//prepare the query
+				$stmt = pg_prepare($conn, "delete_exp", $query);
+				//execute query 
+				$result = pg_execute($conn, "delete_exp", array($val));
+
+				if ($result){
+					echo "\tUpdate was successful. <br />\n";
+					echo "\tReturn to <a href='eExperiments.php'>search page</a>.";
+				}
+				else{
+					echo "\tUpdate FAILED: ".pg_last_error($conn)."<br />\n";
+					echo "\tReturn to <a href='eExperiments.php'>search page</a>.";
+				}
+				break;
+
+			//this action saves the values entered into the edit form to the database
+			case "edit_commit":
+
+				//get postvars
+				$payment = $_POST['payment'];
+				$name = $_POST['name'];
+				$requirements = $_POST['requirements'];
+
+				//define the query to update the city table
+				$query = "UPDATE database.experiments SET (payment, name, requirements) = ($1, $2, $3) WHERE expid = $4";
+				//prepare the query
+				$stmt = pg_prepare($conn, "update_exp", $query);
+				//execute the query with user's values
+				$result = pg_execute($conn, "update_exp", array($payment, $name, $requirements, $val));
+
+				//Check to see if the query was successful
+				if ($result){
+					echo "\tUpdate was successful. <br />\n";
+					echo "\tReturn to <a href='eExperiments.php'>search page</a>.";
+				}
+				else{
+					echo "\tUpdate FAILED: ".pg_last_error($conn)."<br />\n";
+					echo "\tReturn to <a href='eExperiments.php'>search page</a>.";
+				}
+				break;
+
+			//No action selected; show all experiments
+			default:
+				$query = "SELECT * FROM database.experiments ORDER BY expid";
+				//prepare the query
+				$stmt = pg_prepare($conn, "query", $query);
+				//execute the query 
+				$result = pg_execute($conn, "query", array());
+				
+				//Die if the query fails
+				if (!$result){
+					die("Unable to execute query: " . pg_last_error($conn));
+				}
+
+				//Print the results of the query in a nice table
+				pgResultToTableWithButtons($result, "experiments");
+
+				//Print out how many rows were returned
+				echo "\t<p>There were <em>".pg_num_rows($result)."</em> rows returned.</p>\n";
+				break;
+
+
+		}
 
 		
-		//prepare the query
-		$stmt = pg_prepare($conn, "query", $query);
-		//execute the query 
-		$result = pg_execute($conn, "query", array());
+
+	//Prints a table from a pg query result. $type refers to the table from which the result was returned. $val is passed in only for use in building URLs for GET
+	function pgResultsToEditableTableForm($result, $type, $val){
+		include 'connect.php';
 		
-		//Die if the query fails
+		//make sure $result is not null
 		if (!$result){
 			die("Unable to execute query: " . pg_last_error($conn));
 		}
 
-		//Print the results of the query in a nice table
-		pgResultToTableWithButtons($result, "experiments");
-
-		//Print out how many rows were returned
-		echo "\t<p>There were <em>".pg_num_rows($result)."</em> rows returned.</p>\n";
+		//Print form
+		echo "\t<form method='POST' action='eExperiments.php?action=edit_commit&val=".$val."'>\n";
+		//Print table
+		echo "\t<table border='1'>\n";
+		
+		$row = pg_fetch_assoc($result);
+		for ($i = 0; $i < pg_num_fields($result); $i++){
+			echo "\t\t<tr>\n";
+			$fieldname = pg_field_name($result, $i);
+			echo "\t\t\t<td><strong>".$fieldname."</strong></td>\n";
+			echo "\t\t\t<td><input type='text' name='".$fieldname."' value='".$row[$fieldname]."' /></td>";
+	
+			echo "\t\t</tr>\n";
+		}
+		echo "\t</table>\n";
+		echo "\t<input type='submit' value='Save' />\n";
+		echo "\t<input type='button' value='Cancel' onclick=\"top.location.href='eExperiments.php';\" />\n";
+		echo "\t</form>";
+	
+	}
 
 //Function to retrieve experiments data and display it with buttons for user interaction
 function pgResultToTableWithButtons($result, $entryType){
@@ -83,8 +178,8 @@ function pgResultToTableWithButtons($result, $entryType){
 			echo "\t\t<tr>\n";
 			//Print the buttons
 			echo "\t\t\t<td>\n";
-			echo "\t\t\t\t<input type='submit' value='Edit' formaction='eExperiment.php?action=edit&".$buttonAction."' />\n";
-			echo "\t\t\t\t<input type='submit' value='Remove' formaction='eExperiment.php?action=remove&".$buttonAction."' />\n";
+			echo "\t\t\t\t<input type='submit' value='Edit' formaction='eExperiments.php?action=edit&".$buttonAction."' />\n";
+			echo "\t\t\t\t<input type='submit' value='Remove' formaction='eExperiments.php?action=remove&".$buttonAction."' />\n";
 			echo "\t\t\t</td>\n";
 			//Print row contents
 			foreach($row as $entry){
