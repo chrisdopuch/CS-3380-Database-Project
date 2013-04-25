17325f453094ee2a2c6630c93e4848169b87e44b
Chris Dopuch <cdp73@mail.missouri.edu>
2013-04-25 16:37:39 -0500

    Updated eLocations
    Add function still not working

--- eLocations.php
+++ eLocations.php
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
+<!--connect to the database and start the session-->
+<?php include 'connect.php';
+session_start();?>
+
 <head>
 <title>Locations</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -20,6 +24,9 @@
         background-color: white;
         }
 </style>
+
+
+
 </head>
 <body>
 <?php include 'header.php';
@@ -31,118 +38,246 @@ top('experimenter');
         <p>&nbsp;</p>
 
 <?php
-include 'connect.php';
 
-//set error reporting
-ERROR_REPORTING(E_ALL);
-ini_set("display_errors", 1);
 
-//check if already logged in, and redirect
-if(isset($_SESSION['username'])){
-        $user_type = $_SESSION['user_type'];
+$action = $_GET['action']; // edit, remove, edit_commit, NOTHING
+$val = $_GET['val']; //lid
+
+switch ($action)
+	{
+        //this action shows an editable form to the user
+        case "add":
+		$val = 'add';// setting val to add so that it will come back to add after the info has been inserted
+				
+		if ($bool != 1)
+			{
+			$bool = 1;
+			echo"<form action='eLocations.php?action=add&val=".$val."' method='post'>";
+			echo"<table>";
+			//echo"<tr><td>Location ID:</td><td><input type='Integer' name='lid'></td></tr>";
+			echo"<tr><td>Room Number:</td><td><input type='Integer' name='room'></td></tr>";
+			echo"<tr><td>Building Name:</td><td><input type='text' name='building'></td></tr>";
+			echo"<tr><td colspan='1'><input type='submit' value='Submit'></td></tr>";
+			echo"</table>";
+			echo"</form>";
+			}
+		else
+			{
+	                //$lid = $_POST['lid'];
+	                $room = $_POST['room'];
+	                $building = $_POST['building'];
+	        	$query = 'INSERT INTO database.locations (room,building) VALUES ($1, $2)';
+			$result = pg_prepare($conn, "add", $query);
+			$result1 = pg_execute($conn, "add", array($room, $building));
+			
+        
+			if ($result1)
+                	        {
+                	         echo "\tLocation was successfully added. <br />\n";
+                	         echo "\tReturn to <a href='eLocations.php'>search page</a>.";
+                	        }
+                	else
+                	        {
+                	         echo "\tAdding the location FAILED: ".pg_last_error($conn)."<br />\n";
+                	         echo "\tReturn to <a href='eLocations.php'>search page</a>.";
+                	        }
+	
+			$bool = 0;
+			}
+             	break;
+	case "edit":
+	        //define the query to select the location of interest
+                $query = "SELECT * FROM database.locations WHERE lid = $1";
+                //prepare the query
+                $stmt = pg_prepare($conn, "select_lid", $query);
+                //execute query with the desired location code
+                $result = pg_execute($conn, "select_lid", array($val));
+                //print the edit form
+                pgResultsToEditableTableForm($result, $type, $val);
+                break;
+
+	//this action removes the selected experiment from the database
+        case "remove":
+        	$query = "DELETE FROM database.locations WHERE lid = $1";
+                //prepare the query
+                $stmt = pg_prepare($conn, "delete_exp", $query);
+                //execute query 
+                $result = pg_execute($conn, "delete_exp", array($val));
+                if ($result)
+			{
+                         echo "\tUpdate was successful. <br />\n";
+                         echo "\tReturn to <a href='eLocations.php'>search page</a>.";
+                        }
+                else
+			{
+                         echo "\tUpdate FAILED: ".pg_last_error($conn)."<br />\n";
+                         echo "\tReturn to <a href='eLocations.php'>search page</a>.";
+                        }
+                break;
+
+	//this action saves the values entered into the edit form to the database
+        case "edit_commit":
 		
-		//connect to DB
-		include 'connect.php';
+		//get postvars
+		$lid = $_POST['lid'];
+                $room = $_POST['room'];
+                $building = $_POST['building'];
+                //define the query to update the city table
+                $query = "UPDATE database.locations SET (room, building) = ($2, $3) WHERE lid = $1";
+                //prepare the query
+                $stmt = pg_prepare($conn, "update_exp", $query);
+                //execute the query with user's values
+                $result = pg_execute($conn, "update_exp", array($lid,$room, $building));
+                //Check to see if the query was successful
+                if ($result)
+			{
+                         echo "\tUpdate was successful. <br />\n";
+                         echo "\tReturn to <a href='eLocations.php'>search page</a>.";
+                        }
+                else
+			{
+                         echo "\tUpdate FAILED: ".pg_last_error($conn)."<br />\n";
+                         echo "\tReturn to <a href='eLocations.php'>search page</a>.";
+                        }
+                break ;
 
-        //$name = $_POST['query'];
-        $type = 3;      // can be taken out but just used as a continuity check and is used throughout the program
-        $query = "SELECT * FROM database.locations ORDER BY building"; // querry will return all locations and order them by building name
-
-        //if($button == update){   // if update button is depressed, prepare query
-                
-                $result = pg_prepare($conn, 'prepare', $query); // prepare the update query
+	//No action selected; show all experiments
+        default:
+        	$query = "SELECT * FROM database.locations ORDER BY lid";
+                //prepare the query
+                $stmt = pg_prepare($conn, "query", $query);
+                //execute the query 
+                $result = pg_execute($conn, "query", array());
+                //Die if the query fails
                 if (!$result)
-                        {
-                        echo "Error with pg_prepare: " . pg_last_error(); // error checking
-                        exit;
+			{
+                         die("Unable to execute query: " . pg_last_error($conn));
                         }
 
-                 $result = pg_execute($conn, "prepare", array()); // execute the update
-                 if(!$result)
-                        {
-                        echo "Error with pg_execute: " . pg_last_error(); // error checking
-                        exit;
-                        }
-        //        }
-
-		//this needs to be changed
-		//if isset($POST['delete'])
-        else if($button == delete)      // if delete button is depressed, delete the selected row from DB
-		                {
-                $query = "DELETE FROM database.locations WHERE lid = $1";
-                }
+                //Print the results of the query in a nice table
+                pgResultToTableWithButtons($result, "locations");
+                //Print out how many locations were returned
+                echo "\t<p>There were <em>".pg_num_rows($result)."</em> locations returned.</p>\n";
+                break;
+	
+	}//end switch
 
 
-        $result = pg_prepare($conn, "search", $query); // prepare the search query
+
+//Prints a table from a pg query result. $type refers to the table from which the result was returned. $val is passed in only for use in building URLs for GET
+function pgResultsToEditableTableForm($result, $type, $val)
+	{
+	include 'connect.php';
+        //make sure $result is not null
         if (!$result)
-                {
-                echo "Error with pg_prepare: " . pg_last_error(); // error checking
-                exit;
+		{
+                 die("Unable to execute query: " . pg_last_error($conn));
                 }
-		
-        $updatedname = $name . "%";
-        $result = pg_execute($conn, "search", array( $updatedname )); // execute search query
 
-        $counter = 0; // create a counter to count # of locations
+	//Print form
+        echo "\t<form method='POST' action='eLocations.php?action=edit_commit&val=".$val."'>\n";
+        //Print table
+        echo "\t<table border='1'>\n";
+        $row = pg_fetch_assoc($result);
+        for ($i = 0; $i < pg_num_fields($result); $i++)
+		if($i == 0){
+		 echo "\t\t<tr>\n";
+                 $fieldname = pg_field_name($result, $i);
+                 echo "\t\t\t<td><strong>".$fieldname."</strong></td>\n";
+                 echo "\t\t\t<td><input type='text' name='".$fieldname."' value='".$row[$fieldname]."' readonly /></td>";
+                 echo "\t\t</tr>\n";
 
-        echo '<table border="1">';
-        while($row = pg_fetch_assoc($result))   // while loop to send all data to table function
-                {
-                to_table($row, $type); // send to table row and check type
-                $counter = $counter+1; // increment counter
+		}	
+		else{
+                 echo "\t\t<tr>\n";
+                 $fieldname = pg_field_name($result, $i);
+                 echo "\t\t\t<td><strong>".$fieldname."</strong></td>\n";
+                 echo "\t\t\t<td><input type='text' name='".$fieldname."' value='".$row[$fieldname]."' /></td>";
+                 echo "\t\t</tr>\n";
                 }
-        echo "There are " . $counter . " locations. <br /><br />"; // display how man locations are in Database
-        echo '</table>';
+	echo "\t</table>\n";
+        echo "\t<input type='submit' value='Save' />\n";
+        echo "\t<input type='button' value='Cancel' onclick=\"top.location.href='eLocations.php';\" />\n";
+        echo "\t</form>";
 
-}
-///////////////////////// PRINT TABLE FUNCTION///////////////////////////
-function to_table($row, $type)
-        {
-        static $counter = 0;
-        if($counter == 0)
-                {
-                echo '<tr>';
-                echo'<th>Actions</th>';
-                echo '</th>';
-                foreach($row as $key => $value)
-                        {
-                        echo "<th>$key</th>";
+        } // end function
+
+
+
+//Function to retrieve locations data and display it with buttons for user interaction
+function pgResultToTableWithButtons($result, $entryType)
+	{
+        //Print form
+        echo "\t<form method='POST' action='/~cs3380sp13grp11/dataedit_exec.php'>\n";
+        //Print headers
+        echo "\t<table border='1'>\n";
+        echo "\t\t<tr>\n";
+        //print "Actions" header
+        echo "\t\t\t<th>Actions</th>\n";
+	//print the rest of the headers
+        for ($i = 0; $i < pg_num_fields($result); $i++)
+		{
+                 $fieldname = pg_field_name($result, $i);
+                 echo "\t\t\t<th>$fieldname</th>\n";
+                }
+        echo "\t\t</tr>\n";
+        //Print the rows
+        while($row = pg_fetch_assoc($result))
+		{
+                //Prepare buttons
+                switch($entryType)
+			{
+	                case "experimenters":
+                        	$buttonAction = "type=experimenters&val=".$row['eid'];
+                                break;
+                        case "experiments":
+                                $buttonAction = "type=experiments&val=".$row['expid'];
+                                break;
+                        case "participants":
+                                $buttonAction = "type=participants&val=".$row['pid'];
+                                break;
+                        case "sessions":
+                                $buttonAction = "type=sessions&val=".$row['sid'];
+                                break;
+                        case "users":
+                                $buttonAction = "type=users&val=".$row['username'];
+                                break;
+                        case "locations":
+                                $buttonAction = "type=locations&val=".$row['lid'];
+                                break;
+                        default:
+                                echo "Bad option for $entryType in pgResultToTableWithButtons().\n";
+                                exit(1);
                         }
-                echo '</tr>';
-                $counter = $counter + 1;
-                }
-                echo '<tr>';
-                $lid = $row['lid'];
+			
+		echo "\t\t<tr>\n";
+                //Print the buttons
+                echo "\t\t\t<td>\n";
+		echo "\t\t\t\t<input type='submit' name='Edit' value='Edit' formaction='eLocations.php?action=edit&".$buttonAction."' />\n";
+		echo "\t\t\t\t<input type='submit' name='Remove' value='Remove' formaction='eLocations.php?action=remove&".$buttonAction."' />\n";
+	        echo "\t\t\t</td>\n";
+                //Print row contents
+                foreach($row as $entry)
+			{
+			 echo "\t\t\t<td>$entry</td>\n";
+                        }
+                echo "\t\t</tr>\n";
+                } //end while loop
 
-        // delete button        
-        echo "<td><form action='delete.php' onsubmit='return confirmdelete();' method='POST'></a>
-        <input type='submit' name='submit' value= 'Delete' />";
-        echo "<input type='hidden' name='lid' value= $lid />";
-        echo "<input type='hidden' name='type' value=$type /></form>";
-        echo " <div> <input type='hidden' name='confirm' id='confirm' value='false'></div>";
-        echo "</form>";
+	// add button
+	echo "\t\t<tr>\n";
+        //Print the buttons
+        echo "\t\t\t<td>\n";
+        echo "\t\t\t\t<input type='submit' name='Add' value='Add Location' formaction='eLocations.php?action=add&".$buttonAction."' />\n";
 
-        // update button
-        echo "<form action='update.php' method='POST'></a>
-        <input type='submit' name='submit' value= 'Update' />";
-        echo "<input type='hidden' name='lid' value= $lid />";
-        echo "<input type='hidden' name='type' value=$type /></form>";
-        echo '</td>';
 
-        foreach($row as $res)
-                {
-                echo "<td>$res</td>";
-                }
-
-        echo '</tr>';
-        }
+	echo "\t</table>\n";
+        echo "\t</form>";
+        } // end function
 
 ?>
 </div>
-</div>
-
 <!--include the footer-->
-<?php include 'footer.php'; ?>
-
+<!--?php include 'footer.php'; ?-->
 </body>
 </html>
