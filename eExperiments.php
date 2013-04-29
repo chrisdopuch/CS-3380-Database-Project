76efaf5ee13dc6dab0aa2ccdc939e0f24de874bc
Adam Faszl <ajfaszl@mail.missouri.edu>
2013-04-29 17:37:50 -0500

    eExperiments.php integrated with eSessions.php
    There is now a dropdown on experiments that will load all sessions via
    eSessions.php for a selected experiment.
    
    Also made some bugfixes and began implementing the "add experiment"
    function. The form is made but the handler is unfinished. There is no
    link to this new functionality yet.

--- eExperiments.php
+++ eExperiments.php
@@ -17,7 +17,7 @@ top("experimenter");
 	<h2>Experiments</h2><br />
 
 <?php
-		$action = $_GET['action']; // edit, remove, edit_commit, NOTHING
+		$action = $_GET['action']; // edit, remove, edit_commit, add, add_commit, NOTHING
 		$val = $_GET['val']; //expid
 
 		switch ($action){
@@ -77,6 +77,103 @@ top("experimenter");
 				}
 				break;
 
+			//this action displays a form to create a new experiment
+			case "add":
+				//Display add form
+				echo "\t<form method='POST' action='eExperiments.php?action=add_commit'\n";
+				echo "\t\t<input type='hidden' name='action' value='add_commit' />\n";
+				echo "\t\t<p>Enter data for the experiment to be added: </p><br />\n";
+				echo "\t\t<table border='1'>\n";
+				echo "\t\t\t<tr><td>Name</td><td><input type='text' name='name' /></td></tr>\n";
+				echo "\t\t\t<tr><td>Payment</td><td><input type='text' name='payment' /></td></tr>\n";
+				echo "\t\t\t<tr><td>Requirements</td><td><input type='text' name='requirements' /></td></tr>\n";
+				echo "\t\t</table>\n";
+				echo "\t\t<input type='submit' value='Save' />\n";
+				echo "\t\t<input type='button' value='Cancel' onclick=\"top.location.href='eExperiments.php';\" />\n";
+				echo "\t</form>\n";
+
+				echo "\t<form method='POST' action='eExperiments.php?action=add_commit'>\n";
+				echo "\t\t<input type='hidden' name='action' value='add_commit' />\n";
+				echo "\t\t<p>Enter data for the experiment to be added: </p><br />\n";
+				echo "\t\t\t<table border='1'>\n";
+				echo "\t\t\t\t<tr><td>Name</td><td><input type='text' name='name' /></td></tr>\n";
+				echo "\t\t\t\t<tr><td>Payment</td><td><input type='text' name='payment' /></td></tr>\n";
+				echo "\t\t\t\t<tr><td colspan='3'><b>Requirements</b></td></tr>\n";
+				echo "\t\t\t\t<tr><td>Attribute</td><td>Operator</td><td>Selection</td></tr>\n";
+				echo "\t\t\t\t<tr>\n";
+				echo "\t\t\t\t\t<td>Ethnicity</td>\n";
+				echo "\t\t\t\t\t<td>\n";
+				echo "\t\t\t\t\t\t<select name='ethnicity_op'>\n";
+				echo "\t\t\t\t\t\t\t<option value='is'> is </option>\n";
+				echo "\t\t\t\t\t\t\t<option value='is not'> is not </option>\n";
+				echo "\t\t\t\t\t\t</select>\n";
+				echo "\t\t\t\t\t</td>\n";
+				echo "\t\t\t\t\t<td>\n";
+				echo "\t\t\t\t\t\tselect name='ethnicity_sel'>";
+				echo "\t\t\t\t\t\t\t<option value='x' > don't care </option>";
+				echo "\t\t\t\t\t\t\t<option value='s1'> s1 </option>";
+				echo "\t\t\t\t\t\t\t<option value='s2'> s2 </option>";
+				echo "\t\t\t\t\t\t\t<option value='s3'> s3 </option>";
+				echo "\t\t\t\t\t\t</select>";
+				echo "\t\t\t\t\t</td>";
+				echo "\t\t\t\t</tr>";
+
+				echo "\t\t\t\t<tr>\n";
+				echo "\t\t\t\t\t<td>Gender</td>\n";
+				echo "\t\t\t\t\t<td>\n";
+				echo "\t\t\t\t\t\tis\n";
+				echo "\t\t\t\t\t</td>\n";
+				echo "\t\t\t\t\t<td>\n";
+				echo "\t\t\t\t\t\t<select name='gender_sel'>\n";
+				echo "\t\t\t\t\t\t\t<option value='x' > don't care </option>\n";
+				echo "\t\t\t\t\t\t\t<option value='m'> male </option> \n";
+				echo "\t\t\t\t\t\t\t<option value='f'> female </option> \t";
+				echo "\t\t\t\t\t\t</select>\n";
+				echo "\t\t\t\t\t</td>\n";
+				echo "\t\t\t\t</tr>\n";
+
+				echo "\t\t\t\t<tr>\n";
+				echo "\t\t\t\t\t<td>Age</td>\n";
+				echo "\t\t\t\t\t<td>\n";
+				echo "\t\t\t\t\t\t<select name='age_op'>\n";
+				echo "\t\t\t\t\t\t\t<option value='x' > don't care </option>\n";
+				echo "\t\t\t\t\t\t\t<option value='=='> equal to </option> \n";
+				echo "\t\t\t\t\t\t\t<option value='>='> greater than or equal to </option> \n";
+				echo "\t\t\t\t\t\t\t<option value='<='> less than or equal to </option>\n";
+				echo "\t\t\t\t\t\t</select>\n";
+				echo "\t\t\t\t\t</td>\n";
+				echo "\t\t\t\t\t<td>\n";
+				echo "\t\t\t\t\t\t<input type='text' name='age_sel' />\n";
+				echo "\t\t\t\t\t</td>\n";
+				echo "\t\t\t\t</tr>\n";
+
+				echo "\t\t\t\t<tr>\n";
+				echo "\t\t\t\t\t<td>Education</td>\n";
+				echo "\t\t\t\t\t<td>\n";
+				echo "\t\t\t\t\t\t<select name='education_op'>\n";
+				echo "\t\t\t\t\t\t\toption value='x' > don't care </option>\n";
+				echo "\t\t\t\t\t\t\t<option value='=='> equal to </option> \n";
+				echo "\t\t\t\t\t\t\t<option value='>='> greater than or equal to </option> \n";
+				echo "\t\t\t\t\t\t\t<option value='<='> less than or equal to </option>\n";
+				echo "\t\t\t\t\t\t</select>\n";
+				echo "\t\t\t\t\t</td>\n";
+				echo "\t\t\t\t\t<td>\n";
+				echo "\t\t\t\t\t\t<input type='text' name='education_sel' />\n";
+				echo "\t\t\t\t\t</td>\n";
+				echo "\t\t\t\t</tr>\n";
+				echo "\t\t\t</table>\n";
+				echo "\t\t<input type='submit' value='Save' />\n";
+				echo "\t\t<input type='button' value='Cancel' onclick='top.location.href='eExperiments.php'' />\n";
+				echo "\t</form>\n";
+
+				break;
+
+			//this action adds a new experiment to the database based on the data entered by the user
+			case "add_commit":
+
+
+				break;
+
 			//No action selected; show all experiments
 			default:
 				$query = "SELECT * FROM database.experiments ORDER BY expid";
@@ -93,6 +190,17 @@ top("experimenter");
 				//Print the results of the query in a nice table
 				pgResultToTableWithButtons($result, "experiments");
 
+				//make the query to generate the experiment select dropdown
+				$query = "SELECT expid, name FROM database.experiments";
+				//prepare
+				$stmt = pg_prepare($conn, "get_exp_for_dropdown", $query);
+				//execute
+				$result = pg_execute($conn, "get_exp_for_dropdown", array());
+
+				//print out a drop down so the user can select an experiment to view sessions
+				echo "\tSelect an experiment below to view its sessions:\n";
+				pgResultToDropDownWithDefault($result, "experiments", $expid);
+
 				//Print out how many rows were returned
 				echo "\t<p>There were <em>".pg_num_rows($result)."</em> rows returned.</p>\n";
 				break;
@@ -100,97 +208,133 @@ top("experimenter");
 
 		}
 
-		
+	//Inputs
+	//result: the result of a pg query
+	//type: the table the result is from (experimenters, experiments, participants, users, sessions, locations) (not used at present)
+	//default: the value of the primary key field for the one you want to be selected by default
+	//Output
+	//returns: nothing
+	//prints: a drop-down menu with all entries in result, with default selected automatically
+function pgResultToDropDownWithDefault($result, $type, $default){
+	//make sure $result is not null
+	if (!$result){
+		die("Bad value for result in pgResultsToEditableTableForm: " . pg_last_error($conn));
+	}
+	//Print form
+	echo "\t<form method='POST' action='eSessions.php?action=view_exp'>\n";
+	echo "\t\t<select name='expid'>\n";
+
+	//for each row in the query result
+
+	for ($i = 0; $i <= pg_num_fields($result); $i++){
+			//get associative array from query results
+	$row = pg_fetch_assoc($result);
+		//if this entry's expid is set as the default, print it so it will be preselected
+		if ($row['expid'] == $default){
+			echo "\t\t\t<option value='".$row['expid']."' selected='selected'> ".$row['name']." </option>\n";
+		}
+		//otherwise print normal
+		else {
+			echo "\t\t\t<option value='".$row['expid']."' > ".$row['name']." </option>\n";
+		}
+	}
+	//end the select
+	echo "\t\t</select>\n";
+	//add submit button
+	echo "\t\t<input type='submit' name='submit' value='View' />\n";
+	//end the form
+	echo "\t</form>\n";
+}	
 
 	//Prints a table from a pg query result. $type refers to the table from which the result was returned. $val is passed in only for use in building URLs for GET
-	function pgResultsToEditableTableForm($result, $type, $val){
-		include 'connect.php';
-		
-		//make sure $result is not null
-		if (!$result){
-			die("Unable to execute query: " . pg_last_error($conn));
-		}
-
-		//Print form
-		echo "\t<form method='POST' action='eExperiments.php?action=edit_commit&val=".$val."'>\n";
-		//Print table
-		echo "\t<table border='1'>\n";
-		
-		$row = pg_fetch_assoc($result);
-		for ($i = 0; $i < pg_num_fields($result); $i++){
-			echo "\t\t<tr>\n";
-			$fieldname = pg_field_name($result, $i);
-			echo "\t\t\t<td><strong>".$fieldname."</strong></td>\n";
-			echo "\t\t\t<td><input type='text' name='".$fieldname."' value='".$row[$fieldname]."' /></td>";
+function pgResultsToEditableTableForm($result, $type, $val){
 	
-			echo "\t\t</tr>\n";
-		}
-		echo "\t</table>\n";
-		echo "\t<input type='submit' value='Save' />\n";
-		echo "\t<input type='button' value='Cancel' onclick=\"top.location.href='eExperiments.php';\" />\n";
-		echo "\t</form>";
-	
+	//make sure $result is not null
+	if (!$result){
+		die("Unable to execute query: " . pg_last_error($conn));
 	}
+
+	//Print form
+	echo "\t<form method='POST' action='eExperiments.php?action=edit_commit&val=".$val."'>\n";
+	//Print table
+	echo "\t<table border='1'>\n";
+	
+	$row = pg_fetch_assoc($result);
+	for ($i = 0; $i < pg_num_fields($result); $i++){
+		echo "\t\t<tr>\n";
+		$fieldname = pg_field_name($result, $i);
+		echo "\t\t\t<td><strong>".$fieldname."</strong></td>\n";
+		echo "\t\t\t<td><input type='text' name='".$fieldname."' value='".$row[$fieldname]."' /></td>";
+
+		echo "\t\t</tr>\n";
+	}
+	echo "\t</table>\n";
+	echo "\t<input type='submit' value='Save' />\n";
+	echo "\t<input type='button' value='Cancel' onclick=\"top.location.href='eExperiments.php';\" />\n";
+	echo "\t</form>";
+
+}
 
 //Function to retrieve experiments data and display it with buttons for user interaction
 function pgResultToTableWithButtons($result, $entryType){
-		//Print form
-		echo "\t<form method='POST' action='/~cs3380sp13grp11/dataedit_exec.php'>\n";
-		//Print headers
-		echo "\t<table border='1'>\n";
+	//Print form
+	echo "\t<form method='POST' action='/~cs3380sp13grp11/eExperiments.php'>\n";
+	//Print headers
+	echo "\t<table border='1'>\n";
+	echo "\t\t<tr>\n";
+	//print "Actions" header
+	echo "\t\t\t<th>Actions</th>\n";
+	//print the rest of the headers
+	for ($i = 0; $i < pg_num_fields($result); $i++){
+		$fieldname = pg_field_name($result, $i);
+		echo "\t\t\t<th>$fieldname</th>\n";
+	}
+	echo "\t\t</tr>\n";
+
+	//Print the rows
+	while($row = pg_fetch_assoc($result)){
+		//Prepare buttons
+		switch($entryType){
+			case "experimenters":
+				$buttonAction = "type=experimenters&val=".$row['eid'];
+				break;
+			case "experiments":
+				$buttonAction = "type=experiments&val=".$row['expid'];
+				break;
+			case "participants":
+				$buttonAction = "type=participants&val=".$row['pid'];
+				break;
+			case "sessions":
+				$buttonAction = "type=sessions&val=".$row['sid'];
+				break;
+			case "users":
+				$buttonAction = "type=users&val=".$row['username'];
+				break;
+			case "locations":
+				$buttonAction = "type=locations&val=".$row['lid'];
+				break;
+			default:
+				echo "Bad option for $entryType in pgResultToTableWithButtons().\n";
+				exit(1);
+		}
+
 		echo "\t\t<tr>\n";
-		//print "Actions" header
-		echo "\t\t\t<th>Actions</th>\n";
-		//print the rest of the headers
-		for ($i = 0; $i < pg_num_fields($result); $i++){
-			$fieldname = pg_field_name($result, $i);
-			echo "\t\t\t<th>$fieldname</th>\n";
+		//Print the buttons
+		echo "\t\t\t<td>\n";
+		echo "\t\t\t\t<input type='submit' value='View Sessions' formaction='eSessions.php?action=view_exp&val=".$row['expid']."' />\n";
+		echo "\t\t\t\t<input type='submit' value='Edit' formaction='eExperiments.php?action=edit&".$buttonAction."' />\n";
+		echo "\t\t\t\t<input type='submit' value='Remove' formaction='eExperiments.php?action=remove&".$buttonAction."' />\n";
+		echo "\t\t\t</td>\n";
+		//Print row contents
+		foreach($row as $entry){
+			echo "\t\t\t<td>$entry</td>\n";
 		}
 		echo "\t\t</tr>\n";
-
-		//Print the rows
-		while($row = pg_fetch_assoc($result)){
-			//Prepare buttons
-			switch($entryType){
-				case "experimenters":
-					$buttonAction = "type=experimenters&val=".$row['eid'];
-					break;
-				case "experiments":
-					$buttonAction = "type=experiments&val=".$row['expid'];
-					break;
-				case "participants":
-					$buttonAction = "type=participants&val=".$row['pid'];
-					break;
-				case "sessions":
-					$buttonAction = "type=sessions&val=".$row['sid'];
-					break;
-				case "users":
-					$buttonAction = "type=users&val=".$row['username'];
-					break;
-				case "locations":
-					$buttonAction = "type=locations&val=".$row['lid'];
-					break;
-				default:
-					echo "Bad option for $entryType in pgResultToTableWithButtons().\n";
-					exit(1);
-			}
-
-			echo "\t\t<tr>\n";
-			//Print the buttons
-			echo "\t\t\t<td>\n";
-			echo "\t\t\t\t<input type='submit' value='Edit' formaction='eExperiments.php?action=edit&".$buttonAction."' />\n";
-			echo "\t\t\t\t<input type='submit' value='Remove' formaction='eExperiments.php?action=remove&".$buttonAction."' />\n";
-			echo "\t\t\t</td>\n";
-			//Print row contents
-			foreach($row as $entry){
-				echo "\t\t\t<td>$entry</td>\n";
-			}
-			echo "\t\t</tr>\n";
-		}
-
-		echo "\t</table>\n";
-		echo "\t</form>";
 	}
+
+	echo "\t</table>\n";
+	echo "\t</form>";
+}
 ?>
 </div>
 <!--include the footer-->
