2e41f1836a40046ec45db1e8b1ee0785b763267f
Corey Jumper <cjjhd3@mail.missouri.edu>
2013-04-29 02:47:10 -0500

    pExperiment Update

--- pExperiment.php
+++ pExperiment.php
@@ -1,75 +1,92 @@
-<?php
-$connString = "host=dbhost-pgsql.cs.missouri.edu user=cs4320sp13grp14 dbname=cs4320sp13grp14 password=vTc9pwMw"; 
-
-$dbconn = pg_connect($connString ) or die("Problem with connection to PostgreSQL:".pg_last_error());
-
-
-$loggedIn = empty($_SESSION['user']) ? false : $_SESSION['user'];
-if ($loggedIn == false) {
-	header ("Location: index.php");
-	exit;
-	}
-	
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
 <title> Experiments </title>
 <link rel="stylesheet" type="text/css" href="style.css" />
 <style type = "text/css">
-#top
-{
-		text-align: center;
-		border: 1px solid blue;
-		padding-left: 10px;
-		background-color: #FFD651;
-		text-color: black;
-}
-#Options
-{
-	text-align: left;
-	padding-right: 30px;
-	background-color: white;
-{
+
 </style>
 </head>
 <body>
 <?php
 //include header
 include 'header.php';
+include 'connect.php';
+
 top("participant");
-?>
-	<?php
+
 	//get experiment info
-	$expid = $_SESSION[''];
+	$username = $_SESSION['username'];
 	
-	$query1 = "SELECT name, expid FROM experiments WHERE expid = $1";
+	$query1 = "SELECT expid, eid, session_date, start_time, end_time, lid FROM sessions WHERE sid = $1";
 	
-	$result1 = pg_query($dbconn, $query1);
+	$result1 = pg_query($conn, "getres", $query1);
 	$i =0;
 	//populates the table with the tours.
 	while ($row = pg_fetch_assoc($result1))
 	{
 	
-		$name = $row['name'];
-		$expid = $row['expid'];
-		
-		
-		
-				echo "<tr id='row$i'>";
-
-				echo "<td> $name </td>";
+				$exp_id = $row['expid'];
+                        
+				$e_id = $row['eid'];
+				
+				$start_date = $row['session_date'];
+				
+				$start_time = $row['start_time'];
+				
+				$end_time = $row['end_time'];
+				
+				$l_id =$row['lid'];
+				
+				
 						
-				echo "<td> <button onclick='deleteres($expid, $i)'> Delete </button></td>";
+				$query2 = "SELECT exp_id, name FROM experiment WHERE expid = $1";
+				
+				$prep2 = pg_prepare($conn, "exp$i", $query2);
+
+                $result2 = pg_execute($conn, "exp$i", array($e_id));
+
+                $row2 = pg_fetch_assoc ($result2);
+				
+				$exp_name = $row['name'];
+				
+				
+				$query3 = "SELECT building FROM locations WHERE lid = $1";
+				
+				$prep3 = pg_prepare($conn, "locate$i", array($l_id));
+				
+				$row3 = pg_fetch_assoc ($result3);
+				
+				$building_name = $row['building'];
+				
+				
+				
+				
+				echo "<tr id='row$i'>";
+				
+				echo "<td> $exp_name </td>";
+				
+				echo "<td> $e_id </td>";
+				
+				echo "<td> $building_name </td>";
+
+                echo "<td> $start_date </td>";
+				
+                echo "<td> $start_time </td>";
+
+                echo "<td> $end_time </td>";
+						
+				echo "<td> <button onclick='deleteexp($expid, $i)'> Delete </button></td>";
 				
 
 			echo "</tr>";
 $i++;
 	}
+?>
 </body>
 <script>
-//delete experiment row
+
+
 function deleteexp(exp_id, row)
 {
 
@@ -78,7 +95,7 @@ var hiderow = "row" + row;
 $("#" + hiderow).hide();
 
 
-$.post("deletetour.php", 
+$.post("deletesession.php", 
 
 {
 
