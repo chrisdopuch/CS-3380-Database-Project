2a2a027ee47592196accb462aea052548f0cbf69
Breona Butler <breona.butler@gmail.com>
2013-05-05 23:50:22 -0500

    Practice css
    Just something I threw together for an example of the css styling for
    the site

---
+++ practicecss.php
@@ -0,0 +1,142 @@
+<!DOCTYPE>
+<html>
+<title> Practice CSS </title>
+<head>
+<link rel="stylesheet" type="text/css" />
+
+<style>
+#body
+{
+	/* IE10 Consumer Preview */ 
+background-image: -ms-linear-gradient(top, #FFFFFF 0%, #000000 100%);
+
+/* Mozilla Firefox */ 
+background-image: -moz-linear-gradient(top, #FFFFFF 0%, #000000 100%);
+
+/* Opera */ 
+background-image: -o-linear-gradient(top, #FFFFFF 0%, #000000 100%);
+
+/* Webkit (Safari/Chrome 10) */ 
+background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0, #FFFFFF), color-stop(1, #000000));
+
+/* Webkit (Chrome 11+) */ 
+background-image: -webkit-linear-gradient(top, #FFFFFF 0%, #000000 100%);
+
+/* W3C Markup, IE10 Release Preview */ 
+background-image: linear-gradient(to bottom, #FFFFFF 0%, #000000 100%);
+	margin: 0;
+	height:100%;
+}
+
+#text
+{
+	text-align: center;
+}
+
+#text2
+{
+	text-align: center;
+}
+
+#parent
+{
+	font-family: Helvetica;
+	margin: 0px 200px;
+	height: 100%;
+	display:block;
+	text-align: center;
+	background-color: FFD700;
+}
+
+#banner
+{
+	background-color: 00008B;
+	font-color: black;
+	font-size: x-large;
+}
+
+</style>
+
+</head>
+<body>
+	<div id = 'body'>
+<form>
+	
+	<div id = 'parent'>
+	
+	<img src="images\banner.jpg" alt="Header " width =  "1100" height = "100"> 
+	
+	<div id = 'banner'>
+			  <a href="eHome.php">Home</a>
+			  &nbsp;
+			  <a href="eExperiments.php">Experiments</a>
+			  &nbsp;
+			  <a href="eSessions.php">Sessions</a>
+			  &nbsp;
+			  <a href="eLocations.php">Locations</a>
+			  &nbsp;
+			  <a href="eReports.php">Reports</a>
+			  &nbsp;
+			 <a href="eUserInfo.php">My Account</a>
+			 &nbsp;
+			 <a href="dataedit.php">DEBUG: Edit Data</a>
+			 &nbsp;
+			 <a href="logout.php">Logout</a>
+
+	</div>
+</form>
+	
+		<div id = 'text' align = "center">
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+	when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+	It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+	It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+	and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+	when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+	It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+	It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+	and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+	when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+	It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+	It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+	and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	
+	</div>
+
+	<div id = 'text2' align = "center">
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+	when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+	It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+	It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+	and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+	when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+	It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+	It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+	and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+	when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+	It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+	It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+	and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+	when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+	It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+	It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+	and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	</div>
+
+	</div>
+
+	</div>
+</body>
+</html>
