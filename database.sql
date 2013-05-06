5368b5015ec9426feabede7771489246a2785ba2
Adam Faszl <ajfaszl@mail.missouri.edu>
2013-05-06 16:08:22 -0500

    database.sql changed "none" values for...
    Changed "none" values for the inserted experiments to something that's
    valid JSON to avoid erroring out with the validate function in pEnroll.
    Still not sure what's causing the primary key conflict bug with
    experimenters, but I haven't seen it on any other fields yet.

--- database.sql
+++ database.sql
@@ -80,9 +80,9 @@ Insert test data into database for use by developers
 Users are easy enough to be added through registration, and edited on my account page
 same goes for sessions soon
 */
-INSERT INTO experiments (payment, name, requirements) VALUES(150, 'experiment','none');
-INSERT INTO experiments (payment, name, requirements) VALUES(100, 'experiment 1', 'none');
-INSERT INTO experiments (payment, name, requirements) VALUES(50, 'experiment 2', 'none');
+INSERT INTO experiments (payment, name, requirements) VALUES(150, 'experiment','{"none":"none"}');
+INSERT INTO experiments (payment, name, requirements) VALUES(100, 'experiment 1', '{"none":"none"}');
+INSERT INTO experiments (payment, name, requirements) VALUES(50, 'experiment 2', '{"none":"none"}');
 INSERT INTO locations (room, building) VALUES(101, 'Student Center');
 INSERT INTO locations (room, building) VALUES(102, 'McAlester');
 INSERT INTO users VALUES('test_user1', 'password_hash', 'salt', 'experimenter', 'email@mail.com');
