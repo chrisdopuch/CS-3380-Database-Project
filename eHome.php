3b67538ccac02e360ffc4b63893cfbdb3eecedda
Breona Butler <breona.butler@gmail.com>
2013-05-07 11:55:12 -0500

    eHome an pHome code cleaned up

--- eHome.php
+++ eHome.php
@@ -1,96 +1,82 @@
+<!-- Experiments home page allows admin to navigate through various feature. Links in header and 
+in the body are the same. Each link has a description of the pages functionalities. -->
 <!DOCTYPE html>
 <html>
 <head>
 <title> Home </title>
-<link rel="stylesheet" type="text/css" href="style.css" />
-<style type = "text/css">
-#top
-{
-		text-align: center;
-		border: 1px solid blue;
-		padding-left: 10px;
-		background-color: #FFD651;
-		text-color: black;
-}
-#Options
-{
-	text-align: left;
-	padding-right: 30px;
-	background-color: white;
-{
-</style>
+	<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
 <?php
-//include header
-include 'header.php';
-top("experimenter");
+	include 'header.php';
+	top("experimenter");
 ?>
-	<div id = "Options">
+	<!--Experiments desctiption -->
+	<a href = "eExperiments.php ">Experiments</a>
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-View sessions of experiments
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Edit or remove experiments
+		</br>
+		</br>
 	
-				<a href = "eExperiments.php ">Experiments</a>
-					</br>
-							&nbsp;&nbsp;&nbsp;&nbsp;
-					-View sessions of experiments
-					</br>
-							&nbsp;&nbsp;&nbsp;&nbsp;
-					-Edit or remove experiments
-					</br>
-					</br>
-			
-				<a href = "eSessions.php ">Sessions</a>
-					</br>
-							&nbsp;&nbsp;&nbsp;&nbsp;
-					-Edit or remove sessions
-					</br>
-							&nbsp;&nbsp;&nbsp;&nbsp;
-					-Add new sessions
-					</br>
-					</br>
+	<!--Sessions desctiption -->	
+	<a href = "eSessions.php ">Sessions</a>
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Edit or remove sessions
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Add new sessions
+		</br>
+		</br>
 		
-					
-					<a href = "eLocations.php ">Locations</a>
-					</br>
-							&nbsp;&nbsp;&nbsp;&nbsp;
-						-Room and building of experiments
-					</br>
-					</br>
-				
-					<a href = "eReports.php ">Reports</a>
-					</br>
-					&nbsp;&nbsp;&nbsp;&nbsp;
-						-Experiments
-					</br>
-					&nbsp;&nbsp;&nbsp;&nbsp;
-						-Sessions
-					</br>
-					&nbsp;&nbsp;&nbsp;&nbsp;
-						-Participants
-					</br>
-					&nbsp;&nbsp;&nbsp;&nbsp;
-						-Contact List
-					</br>
-					&nbsp;&nbsp;&nbsp;&nbsp;
-						-All experimenters
-					</br>
-					&nbsp;&nbsp;&nbsp;&nbsp;
-						-All users
-					</br>
-					</br>
+	<!--Locations desctiption -->				
+	<a href = "eLocations.php ">Locations</a>
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Room and building of experiments
+		</br>
+		</br>
 		
-						<a href = "eUserInfo.php ">My Account</a>
-						</br>
-								&nbsp;&nbsp;&nbsp;&nbsp;
-							-Change your password
-						</br>
-								&nbsp;&nbsp;&nbsp;&nbsp;
-							-Modify contact information
-						</br>
-								&nbsp;&nbsp;&nbsp;&nbsp;
-							-Change your email address
-						</br>
-						</br>
-
+	<!--Reports desctiption -->
+	<a href = "eReports.php ">Reports</a>
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Experiments
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Sessions
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Participants
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Contact List
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-All experimenters
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-All users
+		</br>
+		</br>
+	
+	<!--My account desctiption -->
+	<a href = "eUserInfo.php ">My Account</a>
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Change your password
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Modify contact information
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Change your email address
+		</br>
+		</br>
 </body>
 </html>
 		
--- pHome.php
+++ pHome.php
@@ -1,87 +1,69 @@
+<!-- Participants home page allows admin to navigate through various feature. Links in header and 
+in the body are the same. Each link has a description of the pages functionalities. -->
+
 <!DOCTYPE html>
 <html>
 <head>
 <title> Home </title>
-<link rel="stylesheet" type="text/css" href="style.css" />
-<style type = "text/css">
-#top
-{
-		text-align: center;
-		border: 1px solid blue;
-		padding-left: 10px;
-		background-color: #FFD651;
-		text-color: black;
-}
-#Options
-{
-	text-align: left;
-	padding-right: 30px;
-	background-color: white;
-{
-</style>
+	<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
 <?php
-//include header
-include 'header.php';
-top("participant");
+	include 'header.php';
+	top("participant");
 ?>
-	<div id = "Options">
-	
-			<div id = "MyStudies">
-			
-				<a href = "pEnroll.php ">Sign Up</a>
-					</br>
-						&nbsp;&nbsp;&nbsp;&nbsp;
-					-View all experiments
-					</br>
-						&nbsp;&nbsp;&nbsp;&nbsp;
-					-Sort by date
-					</br>
-						&nbsp;&nbsp;&nbsp;&nbsp;
-					-Sort by time
-					</br>
-						&nbsp;&nbsp;&nbsp;&nbsp;
-					-Sort by name
-		
-				<div id = "AllStudies">
-					
-					<a href = "pSessions.php ">My Sessions</a>
-					</br>
-							&nbsp;&nbsp;&nbsp;&nbsp;
-						-View all studies available
-					</br>
-					</br>
-		
-					<div id = "MyProfile">
-						<a href = "pUserInfo.php ">My Account</a>
-						</br>
-								&nbsp;&nbsp;&nbsp;&nbsp;
-							-Change your first, middle, and last name
-						</br>
-								&nbsp;&nbsp;&nbsp;&nbsp;
-								-Change your password
-						</br>
-								&nbsp;&nbsp;&nbsp;&nbsp;
-								-Change your email address
-						</br>
-								&nbsp;&nbsp;&nbsp;&nbsp;
-								-Change your address
-						</br>
-								&nbsp;&nbsp;&nbsp;&nbsp;
-								-Change your phone number
-						</br>
-								&nbsp;&nbsp;&nbsp;&nbsp;
-								-Change your ethnicity
-						</br>
-								&nbsp;&nbsp;&nbsp;&nbsp;
-								-Change your age
-						</br>
-								&nbsp;&nbsp;&nbsp;&nbsp;
-								-Change your grade
-						</br>
-						</br>
 
+	<!--Enroll desctiption -->
+	<a href = "pEnroll.php ">Sign Up</a>
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-View all experiments
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Sort by date
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Sort by time
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Sort by name
+		
+	<!--Sessions desctiption -->
+	<a href = "pSessions.php ">My Sessions</a>
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-View all studies available
+		</br>
+		</br>
+		
+	<!--My Account desctiption -->
+	<a href = "pUserInfo.php ">My Account</a>
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Change your first, middle, and last name
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Change your password
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Change your email address
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Change your address
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Change your phone number
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Change your ethnicity
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Change your age
+		</br>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+				-Change your grade
+		</br>
+		</br>
 </body>
 </html>
 		
